5027ed69b2034ff5a41c36057ba9fca3775bc938
Zheyuan Xie <zyxie.sjtu@gmail.com>
2019-11-03 17:13:02 -0500

    keyboard support

--- src/main.js
+++ src/main.js
@@ -18,14 +18,30 @@ setRenderer(params.renderer);
 function setRenderer(renderer) {
   switch(renderer) {
     case FORWARD:
+      params.renderer = FORWARD;
       params._renderer = new ForwardRenderer();
       break;
     case FORWARD_PLUS:
+      params.renderer = FORWARD_PLUS;
       params._renderer = new ForwardPlusRenderer(15, 15, 15);
       break;
     case CLUSTERED:
+      params.renderer = CLUSTERED;
       params._renderer = new ClusteredRenderer(15, 15, 15);
       break;
+  }
+}
+
+document.body.onkeypress = function(e){
+  if(e.keyCode == 49){            // 1
+    setRenderer(FORWARD);
+    console.log("FORWARD");
+  } else if (e.keyCode == 50) {   // 2
+    setRenderer(FORWARD_PLUS);
+    console.log("FORWARD_PLUS");
+  } else if (e.keyCode == 51) {   // 3
+    setRenderer(CLUSTERED);
+    console.log("CLUSTERED");
   }
 }
 
